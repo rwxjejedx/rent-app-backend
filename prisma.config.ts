3994f43d22896d653b7cc298c98d8a987a9ecef1
maulanajulham <julhammaulana@gmail.com>
2026-05-03 09:40:16 +0700

    dojne

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,18 @@
+// import 'dotenv/config';
+// import { defineConfig, env } from 'prisma/config';
+
+// export default defineConfig({
+//   schema: 'prisma/schema.prisma',
+//   migrations: {
+//     path: 'prisma/migrations',
+//   },
+//   datasource: {
+//     url: env('DATABASE_URL'),
+//   },
+// });
+
 import 'dotenv/config';
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from '@prisma/config'; // Perhatikan import ini
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -7,6 +20,8 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    // Gunakan process.env standar dengan fallback string kosong atau dummy
+    // Ini mencegah PrismaConfigEnvError saat build di Railway
+    url: process.env.DATABASE_URL || "postgresql://unused:unused@localhost:5432/unused",
   },
 });
