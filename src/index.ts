99c3d34015228512d1d1091d8dcd9b8054ca3d80
maulanajulham <julhammaulana@gmail.com>
2026-05-04 08:11:50 +0700

    feat: initialize express server with routes, CORS configuration, and error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Izinkan hanya frontend kamu
-  methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH', 'OPTIONS'],
+  origin: ['http://localhost:5173', 'https://rent-app-frontend.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Penting jika kamu nanti menggunakan cookie/session
 }));
